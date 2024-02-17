37585ffd5c8f73c8039f8118935433429f4e86b5
alina <alina.rashid2002@gmail.com>
2024-02-17 17:01:22 +0400

    created a function called changeColor that changes the color of the element (grid div) calling it to grey whenever a mouse is hovered over it

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ for(let i=0; i<16; i++){
     for(let j=0; j<16; j++){
      var grid=document.createElement('div');
      grid.classList.add('grid');
+     grid.addEventListener('mouseover',changeColor); 
      container.append(grid);
     }
 }
 
-container.style.cssText='';
+//e represented the grid div calling the function.
+function changeColor(e){
+    e.target.style.backgroundColor = 'grey'; // Corrected the style assignment
+}
